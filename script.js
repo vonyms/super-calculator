01feccc9d967c1b8792c71862763fc4dbdd8370f
vonyms <vonirandria07@gmail.com>
2023-07-18 05:49:02 +0300

    update script

--- script.js
+++ script.js
@@ -1,63 +1,32 @@
-let time = document.querySelector('.block-calculator__int-top-time');
-let item = document.querySelectorAll('.item');
-let result = document.querySelector('.value-result');
-let clear = document.querySelector('.clear');
-let numeric = document.querySelectorAll('.numeric');
-let operand = document.querySelectorAll('.operand');
-let equal = document.querySelector('.equal');
-let temp1 = 0; 
-let temp2 = 0;
-let operandSign = '';
-let setNegativeValue = document.querySelector('.setNegative');
-time.textContent = new Date().getHours()+':'+new Date().getMinutes();
+let result = '';
+let operator = '';
+let operand = '';
 
-let clearValue = () => {
-    result.textContent = 0;
+function clearResult() {
+  result = '';
+  operator = '';
+  operand = '';
+  document.getElementById('result').value = '';
 }
-clear.addEventListener('click', clearValue());
 
-item.forEach((e) => {
-    e.addEventListener('click', (event) => {
-        if(event.target.classList.contains('numeric')) {
-            result.textContent += event.target.textContent.trim();
-            
-            console.log('numeric '+event.target.textContent.trim());
-        }
-        if(event.target.classList.contains('operand')) {
-            console.log('operand '+event.target.textContent.trim());
-            operandSign = event.target.textContent.trim();
-            temp1 = result.textContent;
-            
-            clearValue();
-        }
-        if(event.target.classList.contains('equal')) {
-            console.log('click sur '+event.target.textContent.trim());
-            
-            if(operandSign == '*') {
-                temp2 = result.textContent;
-                result.textContent = (temp1 * temp2);
-            }
-            if(operandSign == '/') {
-                temp2 = result.textContent;
-                result.textContent = temp1 / temp2;
-            }
-            if(operandSign == '-') {
-                temp2 = result.textContent;
-                result.textContent = temp1 - temp2;
-            }
-            if(operandSign == '+') {
-                temp2 = result.textContent;
-                result.textContent = temp1 + temp2;
-            }
-            if(operandSign == '%') {
-                temp2 = result.textContent;
-                result.textContent = temp1 % temp2;
-            }
-        }
-    });
-});
-
-let setNegative = () => {
-    result.textContent *= (-1); 
+function appendToResult(value) {
+  result += value;
+  document.getElementById('result').value = result;
 }
-setNegativeValue.addEventListener('click', setNegative)
+
+function calculate(op) {
+  if (op === '=') {
+    if (result && operator && operand) {
+      result = eval(operand + operator + result);
+      operator = '';
+      operand = '';
+      document.getElementById('result').value = result;
+    }
+  } else if (op === '/' || op === '*' || op === '-' || op === '+') {
+    if (result && !operator) {
+      operator = op;
+      operand = result;
+      result = '';
+    }
+  }
+}
